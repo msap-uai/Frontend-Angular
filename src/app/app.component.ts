38109e598772bb9a1d1239611a8e79d2dadec086
msap-uai <manuelsaponaro.uai@gmail.com>
2022-07-18 22:06:56 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,8 @@ export class AppComponent {
       const user = this.storageService.getUser();
       this.roles = user.roles;
 
-      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.showAdminBoard = this.roles.includes('ADMIN');
+      this.showModeratorBoard = this.roles.includes('MODERATOR');
 
       this.username = user.username;
     }
